a7c69906c1c0c1fc6b3745024a11aec97af80902
thakurakhilesh1998 <39956027+thakurakhilesh1998@users.noreply.github.com>
2020-09-15 21:06:43 +0530

    add input box

--- src/CreateList.jsx
+++ src/CreateList.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './CreateList.css';
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
 
 const CreateList=()=>{
     return(
@@ -9,6 +11,7 @@ const CreateList=()=>{
                 <form>
                     <input type="text" placeholder="Title"/><br/>
                     <textarea placeholder="Write a note"></textarea>
+                    <Fab> <AddIcon/></Fab>
                 </form>
             </div>
         </>
