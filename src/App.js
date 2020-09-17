f110b655de2dcddc6ada273ad8056979bc32d319
thakurakhilesh1998 <39956027+thakurakhilesh1998@users.noreply.github.com>
2020-09-17 19:49:06 +0530

    list

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './Header';
 import Footer from './Footer';
 import CreateList from './CreateList';
+import {KeepList} from './KeepList';
 
 function App() {
   return (
     <>
       <Header></Header>
       <CreateList/>
+      <KeepList/>
       <Footer></Footer>
     </>
   );
---
+++ src/KeepList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './KeepList.css';
+
+const KeepList=()=>{
+    return(
+
+        <>
+            <div className="list">
+                <h3>Title</h3>
+                <p>Keep it as takes it the Keep it as takes it the.Keep it as takes it the
+                Keep it as takes it the</p>
+            </div>
+        </>
+    );
+}
+
+export {KeepList}
